d582d18767cab38ce5b2d7ea891ace503bce83d2
Axel Mrak <axelmrak@gmail.com>
2025-05-13 22:26:45 -0300

    add autocmds and options

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,4 +1,38 @@
--- Autocmds are automatically loaded on the VeryLazy event
-local opt = vim.opt
-vim.opt.termguicolors = true
-opt.wrap = true
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*",
+  callback = function()
+    vim.lsp.buf.format({ async = false })
+  end,
+})
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+  callback = function()
+    vim.highlight.on_yank({ timeout = 200 })
+  end,
+})
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*",
+  callback = function()
+    vim.cmd([[%s/\s\+$//e]])
+  end,
+})
+
+vim.api.nvim_create_autocmd("BufReadPost", {
+  callback = function()
+    local line = vim.fn.line
+    if line("'\"") > 1 and line("'\"") <= line("$") then
+      vim.cmd('normal! g`"')
+    end
+  end,
+})
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*",
+  callback = function()
+    local dir = vim.fn.expand("<afile>:p:h")
+    if vim.fn.isdirectory(dir) == 0 then
+      vim.fn.mkdir(dir, "p")
+    end
+  end,
+})
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,1 +1,53 @@
 -- Options are automatically loaded before lazy.nvim startup
+local opt = vim.opt
+opt.termguicolors = true
+opt.wrap = true
+
+vim.diagnostic.config({
+  virtual_text = true,
+  signs = true,
+  underline = true,
+  update_in_insert = false,
+  severity_sort = true,
+})
+
+-- 🧹 Interfaz limpia
+opt.number = true -- muestra números de línea
+opt.relativenumber = true -- muestra números relativos
+opt.cursorline = true -- resalta la línea actual
+opt.signcolumn = "yes" -- siempre muestra la columna de signos (LSP, Git)
+opt.termguicolors = true -- soporte para más colores
+
+-- 🧠 Navegación y edición
+opt.scrolloff = 8 -- mantiene espacio arriba/abajo al hacer scroll
+opt.sidescrolloff = 8
+opt.wrap = false -- no hacer wrap de líneas largas
+opt.linebreak = true -- si usás wrap, que corte por palabra
+
+-- 🧼 Tabs
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true -- convierte tabs a espacios
+opt.smartindent = true
+
+-- 🔍 Búsqueda
+opt.ignorecase = true
+opt.smartcase = true
+opt.incsearch = true
+opt.hlsearch = false
+
+-- 🕹️ UX general
+opt.mouse = "a" -- habilita mouse
+opt.clipboard = "unnamedplus" -- sistema clipboard
+opt.updatetime = 300 -- mejora el tiempo de respuesta en plugins
+opt.timeoutlen = 300 -- velocidad para comandos de tecla
+
+-- 📝 Persistencia
+opt.undofile = true -- guarda undo entre sesiones
+
+-- 🧩 Autocompletado
+opt.completeopt = { "menu", "menuone", "noselect" }
+
+-- 🧼 Invisibles
+opt.list = true
+opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
