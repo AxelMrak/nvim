e400d37b58135d0d606c1728daf17692c05b996c
Axel Mrak <axelmrak@gmail.com>
2025-05-13 22:31:53 -0300

    fix braces plugins

--- lua/plugins/template-string.lua
+++ lua/plugins/template-string.lua
@@ -1,24 +1,26 @@
 return {
-  "axelvc/template-string.nvim",
-  config = function()
-    require("template-string").setup({
-      filetypes = {
-        "html",
-        "typescript",
-        "javascript",
-        "typescriptreact",
-        "javascriptreact",
-        "vue",
-        "svelte",
-        "python",
-        "cs",
-      },
-      jsx_brackets = true,
-      remove_template_string = false,
-      restore_quotes = {
-        normal = [[']],
-        jsx = [["]],
-      },
-    })
-  end,
+  {
+    "axelvc/template-string.nvim",
+    config = function()
+      require("template-string").setup({
+        filetypes = {
+          "html",
+          "typescript",
+          "javascript",
+          "typescriptreact",
+          "javascriptreact",
+          "vue",
+          "svelte",
+          "python",
+          "cs",
+        },
+        jsx_brackets = true,
+        remove_template_string = false,
+        restore_quotes = {
+          normal = [[']],
+          jsx = [["]],
+        },
+      })
+    end,
+  },
 }
