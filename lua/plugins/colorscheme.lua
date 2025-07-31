cb26af127274c90b5881f5885a663a3806652405
Axel Mrak <axelmrak@gmail.com>
2025-07-31 12:51:24 -0300

    change colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,25 +1,45 @@
 return {
   {
-    "catppuccin/nvim",
-    name = "catppuccin",
+    "tiesen243/vercel.nvim",
+    lazy = false,
     priority = 1000,
     config = function()
-      require("catppuccin").setup({
-        transparent_background = false,
-        color_overrides = {
-          all = {
-            base = "#080808",
-            crust = "#1B1B1B",
-            mantle = "#0C0C0C",
-          },
+      require("vercel").setup({
+        theme = "dark",
+        transparent = false,
+        italics = {
+          comments = true,
+          keywords = true,
+          functions = true,
+          strings = true,
+          variables = true,
+          bufferline = false,
+        },
+        overrides = {
+          Normal = { bg = "#000000" },
+          NormalFloat = { bg = "#000000" },
+          FloatBorder = { bg = "#000000", fg = "#30363d" },
+          CursorLine = { bg = "#111111" },
+          CursorLineNr = { fg = "#00eaff", bg = "#111111" },
+          SignColumn = { bg = "#000000" },
+          StatusLine = { bg = "#000000", fg = "#00eaff" },
+          StatusLineNC = { bg = "#000000", fg = "#333333" },
+          TabLine = { bg = "#000000", fg = "#ffffff" },
+          Visual = { bg = "#009dff", fg = "#000000" },
+          Search = { bg = "#ffaa00", fg = "#000000" },
+          Error = { fg = "#ff3b30", bg = "#000000" },
+          WarningMsg = { fg = "#ffaa00", bg = "#000000" },
+          DiffAdd = { bg = "#003300" },
+          DiffDelete = { bg = "#330000" },
         },
       })
+      vim.cmd.colorscheme("vercel")
     end,
   },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin",
+      colorscheme = "vercel",
     },
   },
 }
